6b890a3253952a52d09604ff80781e9c72d2c9c4
Felix Owusu <felixo6996@gmail.com>
2025-12-03 14:14:37 +0000

    about page , controllers , privacy and contact page have been added

--- StudentSubmissionPortal/Controllers/HomeController.cs
+++ StudentSubmissionPortal/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace StudentSubmissionPortal.Controllers
             return View();
         }
 
+        public IActionResult Contact() {
+            return View();
+        }   
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
